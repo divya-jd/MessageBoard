e59a6fb22528c0dbd5d7e2db4d673953742b4a1a
divya-jd <gorla.joise@gmail.com>
2024-11-13 16:41:02 -0500

    added settings_screen

---
+++ lib/settings_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class SettingsScreen extends StatelessWidget {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  Future<void> _logout(BuildContext context) async {
+    await _auth.signOut();
+    Navigator.pushReplacementNamed(context, '/login');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Settings'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              onPressed: () => _logout(context),
+              child: Text('Log Out'),
+            ),
+            Text('Other settings options here'),
+          ],
+        ),
+      ),
+    );
+  }
+}
