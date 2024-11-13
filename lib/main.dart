e15e3045a0ed8bb3fb6f11d32cd85e3a6b96abc2
divya-jd <gorla.joise@gmail.com>
2024-11-13 16:49:11 -0500

    added routes to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,31 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'splash_screen.dart';
+import 'login_screen.dart';
+import 'registration_screen.dart';
+import 'message_board_screen.dart';
+import 'chat_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Message Board App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to the Message Board App'),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => SplashScreen(),
+        '/login': (context) => LoginScreen(),
+        '/register': (context) => RegistrationScreen(),
+        '/messageBoards': (context) => MessageBoardScreen(),
+        '/chat': (context) => ChatScreen(
+          boardName: ModalRoute.of(context)!.settings.arguments as String,
         ),
-        body: Center(
-          child: Text('Hello, Firebase!'),
-        ),
-      ),
+      },
     );
   }
 }
-
--- lib/message_board_screen.dart
+++ lib/message_board_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'chat_screen.dart'; 
 class MessageBoardScreen extends StatelessWidget {
   final List<Map<String, String>> messageBoards = [
     {'title': 'General Chat', 'icon': 'assets/general_icon.png'},
